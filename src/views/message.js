953a74356b51e0e8a1614c0bb425d87e0f1ce62c
Rory Groenewege <rory0172@live.nl>
2019-11-11 17:48:59 +0100

    Fixed styling code.

--- src/views/message.js
+++ src/views/message.js
@@ -5,25 +5,24 @@ let elements = [];
 let body;
 
 export function show(subdomain) {
-    // convert plain HTML string into DOM elements
-    let temporary = document.createElement('div');
-    temporary.innerHTML = html;
-    temporary.getElementsByTagName('iframe')[0].src = `http://${subdomain}.legalsites.app/widget`
-    let dialog = temporary.getElementsByClassName('js-widget-dialog')[0];
-    dialog.style.cssText = "margin-left: 50%;"
+  let temporary = document.createElement('div');
+  temporary.innerHTML = html;
+  temporary.getElementsByTagName('iframe')[0].src = `http://${subdomain}.legalsites.app/widget`;
+  let dialog = temporary.getElementsByClassName('js-widget-dialog')[0];
+  dialog.style.cssText = 'margin-left: 50%;';
 
-    body = document.getElementsByTagName('body')[0];
+  body = document.getElementsByTagName('body')[0];
 
-    while (temporary.children.length > 0) {
-        elements.push(temporary.children[0]);
-        body.appendChild(temporary.children[0]);
-    }
-    body.addEventListener('click', close);
+  while (temporary.children.length > 0) {
+    elements.push(temporary.children[0]);
+    body.appendChild(temporary.children[0]);
+  }
+  body.addEventListener('click', close);
 }
 
 export function close() {
-    while (elements.length > 0) {
-        elements.pop().remove();
-    }
-    body.removeEventListener('click', close);
+  while (elements.length > 0) {
+    elements.pop().remove();
+  }
+  body.removeEventListener('click', close);
 }
